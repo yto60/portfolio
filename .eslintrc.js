0c063f72a3e5a8886404827e7dcdc59830d5f491
60° <hatsumi.yamane.4609@gmail.com>
2020-01-29 23:35:24 +0900

    Nuxt.js init

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/recommended',
+    'plugin:prettier/recommended'
+  ],
+  // *.vue ファイルを lint にかけるために必要
+  plugins: [
+    'vue'
+  ],
+  // ここにカスタムルールを追加します。
+  rules: {
+    'vue/max-attributes-per-line': 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'prettier/prettier': [
+      'warn',
+      {
+        singleQuote: true,
+        semi: false,
+        tabWidth: 2
+      }
+    ]
+  }
+}
---
+++ README.md
@@ -0,0 +1,22 @@
+# yto60
+
+> portfolio
+
+## Build Setup
+
+``` bash
+# install dependencies
+$ npm run install
+
+# serve with hot reload at localhost:3000
+$ npm run dev
+
+# build for production and launch server
+$ npm run build
+$ npm run start
+
+# generate static project
+$ npm run generate
+```
+
+For detailed explanation on how things work, check out [Nuxt.js docs](https://nuxtjs.org).
