94b92f90b41a37fc7100860283cf6dd1151819fe
60° <hatsumi.yamane.4609@gmail.com>
2020-02-07 06:35:24 +0900

    work-details

--- pages/works/_id.vue
+++ pages/works/_id.vue
@@ -1,12 +1,22 @@
 <template lang="pug">
-  div.works-id
+  div.works-id(:class="$store.getters.getViewTypeClass")
     div.works-list-wrapper
       WorksList(:selectedIndex="id" v-if="$store.state.viewType === 'desktop'")
-    //- div.work-details
-      //- img()
-      h4
-        | {{ work.name }}
-      div.description(v-html="work.description")
+    div.work-details-wrapper
+      div.work-details
+        img(:src="`/img/${work.logo}`").logo
+        h3
+          | {{ work.name }}
+        div.description(v-html="work.description")
+        div.links
+          span(v-for="link in work.links").link
+            | {{ link.summary }}
+            a(:href="link.url" target="_blank")
+              ion-icon(name="open")
+        div.sub-images
+          div.image(v-for="image in work.images")
+            img(:src="`/img/${image.url}`")
+            div(v-if="image.description" v-html="image.description").image-description
 
 </template>
 
@@ -35,10 +45,63 @@ export default class WorksId extends Vue {
 </script>
 
 <style lang="scss" scoped>
+.works-id {
+  width: 100%;
+}
 .works-list-wrapper {
   height: 100%;
   width: min-content;
-  padding-right: 0.2rem;
-  border-right: 3px solid $main-blue;
+  float: left;
+}
+.work-details-wrapper {
+  float: right;
+  width: 95%;
+  text-align: center;
+  margin-top: 2rem;
+  .work-details {
+    img.logo {
+      width: 50%;
+    }
+    .description {
+      text-align: left;
+    }
+    .links {
+      margin-top: 1rem;
+      text-align: right;
+      .link {
+        color: $base-darkgray;
+      }
+    }
+    .sub-images {
+      margin-top: 1rem;
+      .image {
+        margin: 1rem auto;
+        width: 90%;
+        max-width: 430px;
+        img {
+          width: 100%;
+        }
+        .image-description {
+          font-size: 0.8rem;
+          color: $base-darkgray;
+          margin-top: 0.3rem;
+        }
+      }
+    }
+  }
+}
+.is-desktop {
+  .works-list-wrapper {
+    padding-right: 0.2rem;
+    border-right: 3px solid $main-blue;
+  }
+  .work-details-wrapper {
+    width: 62%;
+    max-width: calc(100% - 276px - 3rem);
+    .work-details {
+      width: 90%;
+      margin: auto;
+    }
+  }
 }
 </style>
